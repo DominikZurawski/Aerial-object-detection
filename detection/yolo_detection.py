e4868c8ad6dc8fac190dafa3dbf5f313700b64e0
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2025-03-27 12:21:16 +0100

    add movie exp1

--- detection/yolo_detection.py
+++ detection/yolo_detection.py
@@ -1,47 +1,49 @@
 import cv2
 from ultralytics import YOLO
 
-# Inicjalizacja modelu YOLOv8
-model = YOLO('last.pt')  # Ścieżka do Twojego wytrenowanego modelu
+model = YOLO('detection/last.pt')
 
-# Funkcja do przetwarzania wideo
 def process_video(source):
     cap = cv2.VideoCapture(source)
+
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # Kodek wideo
+    out = cv2.VideoWriter('detection/output.avi', fourcc, fps, (frame_width, frame_height))
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Detekcja obiektów
         results = model(frame)
 
-        # Rysowanie wyników na klatce
         for r in results:
             boxes = r.boxes
             for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0]
-                conf = box.conf[0]
-                cls = int(box.cls[0])
+                if box.conf > 0.5:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.putText(frame,
+                              f'{model.names[int(box.cls)]} {box.conf[0]:.2f}',
+                              (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                              0.9, (0, 255, 0), 2)
 
-                if conf > 0.5:  # Próg pewności
-                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-                    cv2.putText(frame, f'{model.names[cls]} {conf:.2f}',
-                                (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                                0.9, (0, 255, 0), 2)
-
+        out.write(frame)
         cv2.imshow('Object Detection', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
-# Wybór źródła wideo
 # source = 0  # 0 dla kamery, lub ścieżka do pliku wideo, np. 'path/to/video.mp4'
 # source = 'DronevsBird.mp4'
 # source = 'Eagleattacks.mp4'
-source = 'Airshow2024.mp4'
+source = 'detection/movie.mp4'
 # source = 'BalloonFiesta.mp4'
 process_video(source)
